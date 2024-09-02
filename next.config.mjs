72c2f41d3ef5c50269697535d693c84548bf9421
Riturajsharma707 <riturajsharma54321@gmail.com>
2024-09-02 10:06:31 +0530

    final commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  env: {
+    API_URL: process.env.API_URL,
+    API_KEY: process.env.API_KEY,
+  },
 };
 
 export default nextConfig;
